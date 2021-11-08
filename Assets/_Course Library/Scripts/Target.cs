1fa2f19cb877a65e11de28e276625ba3962d552c
flamo939beast <23schua@live.elida.k12.oh.us>
2021-11-08 08:24:50 -0500

    Scorekeeperissues

---
+++ Assets/_Course Library/Scripts/Target.cs
@@ -0,0 +1,34 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Target : MonoBehaviour
+{
+    public GameObject Dart;
+    public TextMeshProUGUI scoreText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Dart"))
+        {
+            Destroy(gameObject);
+
+            
+
+            scoreText.GetComponent<ScoreKeeper>().scoreValue += 5;
+            scoreText.GetComponent<ScoreKeeper>().UpdateScore();
+        }
+    }
+}
---
+++ Assets/_Course Library/Scripts/targetspawning.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetspawning : MonoBehaviour
+{
+    public GameObject Jacob;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Jacob, new Vector3(Random.Range(6.0f, 10.0f), Random.Range(1.0f, 2.0f), Random.Range(2.0f, 2.025f)), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
